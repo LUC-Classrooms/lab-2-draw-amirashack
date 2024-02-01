b926acab46b0cc06db027eb6c85fe8a8c2a748a4
amirashack <156958322+amirashack@users.noreply.github.com>
2024-02-01 17:25:28 +0000

    good

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function setup(){
-  createCanvas(600, 400);
+  createCanvas(800, 600);
+
 }
 
 function draw(){
-  background(200);
-  // add your drawing code here ...
-  fill(255, 200, 50)
-  ellipse(150, 50, 50)
-
+  background(200, 225, 255);
+  fill(255, 230, 50);
+  strokeWeight(1.75);
+  ellipse(750, 50, 150, 150);
+  strokeWeight(3);
+  fill(140, 0, 20)
+  rect(420, 368, 240, 230);
+  fill(220, 115, 65);
+  triangle(375, 368, 698, 367, 535, 265);
+  fill(155, 73, 0);
+  rect(480, 537, 45, 60);
+  fill(255, 230, 50);
+  ellipse(490, 575, 10, 15);
+  rect(440, 400, 60, 60);
+  line(468, 458, 468, 400);
+  line(440, 431, 500, 431);
+  rect(580, 400, 60, 60);
+  line(580, 431, 640, 431);
+  line(609, 460, 609, 400);
+  rect()
+  
 }
